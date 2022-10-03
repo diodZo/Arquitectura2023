97b83c3a58aaefca2214cbf605fadbd35df52f8a
jorgetinto <jpinogaray@gmail.com>
2022-10-02 23:40:48 -0300

    commit inicial

---
+++ Infrastructure/Migrations/20221002120914_prueba.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class prueba : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ReferenceKey = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppSettings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppSettings");
+        }
+    }
+}
---
+++ Infrastructure/Persistence/ApplicationDBContext.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class ApplicationDBContext : DbContext, IApplicationDBContext
+    {
+        #region Ctor
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+         : base(options)
+        {
+        }
+        #endregion
+
+        #region DbSet
+        public DbSet<AppSetting> AppSettings { get; set; }
+        #endregion
+
+        #region Methods
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+        #endregion
+    }
+}

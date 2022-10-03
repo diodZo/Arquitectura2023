cbbcecd5ce6b72d27f2806ebd255729005c73323
jpinogaray <jpino@hsalvador.cl>
2022-10-03 14:57:28 -0300

    .

--- WebApiAuth/Controllers/AuthenticateController.cs
+++ WebApiAuth/Controllers/AuthenticateController.cs
@@ -4,7 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using WebApiAuth.Core.Domain.Models;
+using WebApiAuth.Core.Domain.ViewModels;
 
 namespace WebApiAuth.Controllers
 {
